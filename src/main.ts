458859e4349c54374f857cacd32a70a625e2a5e3
Marcos Rabinowicz <marcosrabinowicz@hotmail.com>
2026-01-28 01:51:36 -0300

    feat(api): enables CORS for integration with the local frontend
    - Enables CORS in the application's bootstrap
    - Authorizes localhost and 127.0.0.1 origins for the development environment
    - Prepares an API for consumption by the Angular frontend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { DomainExceptionFilter } from './shared/filters/domain-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
